2f89431fdc4c679f5490ce05bf7869c8c6b05d75
Adekunlea917 <adetunji.kaseem@zentricanalytics.com>
2026-07-27 01:45:36 -0700

    Refine careers hero responsive height

--- src/app/careers/page.tsx
+++ src/app/careers/page.tsx
@@ -82,7 +82,7 @@ export default function Careers() {
   return (
     <PageShell>
       <div className="bg-[#F3F6F9]">
-        <section className="relative isolate flex h-auto min-h-[420px] flex-col justify-center overflow-hidden bg-[#0B1F3A] px-4 py-8 sm:h-[480px] sm:min-h-[480px] sm:max-h-[480px] sm:px-6 sm:py-10 lg:h-[520px] lg:min-h-[520px] lg:max-h-[520px] lg:px-8 lg:py-12" aria-labelledby="careers-hero-heading">
+        <section className="relative isolate flex h-[470px] flex-col justify-center overflow-hidden bg-[#0B1F3A] px-4 py-8 sm:h-[500px] sm:px-6 sm:py-10 lg:h-[540px] lg:px-8 lg:py-12" aria-labelledby="careers-hero-heading">
           <Image
             src="/images/careers/careers-team-collaboration.png"
             sizes="100vw"
